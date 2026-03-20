6ee31496cb1e9329bac6cf9b7acbb82957283869
median-dxz <median.dxz@gmail.com>
2026-03-20 21:42:54 +0800

    bump dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-        presets: [],
-      },
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset({ compilationMode: 'annotation' })] })],
+
+  resolve: {
+    tsconfigPaths: true,
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
